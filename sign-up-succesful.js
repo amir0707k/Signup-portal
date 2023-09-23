be3b01dd30a38a9f15fe8767c03d87068c9ece49
amir0707k <amirk0707@gmail.com>
2023-09-23 21:35:28 +0530

    first commit

---
+++ sign-up-succesful.js
@@ -0,0 +1,14 @@
+let userData = {};
+userData = JSON.parse(localStorage.getItem("userData"));
+console.log(userData);
+const userInfo = document.querySelector(".user-info");
+userInfo.innerHTML = `
+    <p>Full Name: ${userData.name}</p>
+    <p>Email: ${userData.email}</p>
+    <p>Token: ${userData.token}</p>
+    <p>Password: ${userData.password}</p>
+`;
+document.getElementById("logout").addEventListener("click",()=>{
+    localStorage.removeItem('userData');
+    window.location.href = "/index.html"
+});
